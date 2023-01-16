301fced7bd6c5da2c2a7706c3e07ccdbbbd25455
Izumiyayu <m0a2102235@edu.teu.ac.jp>
2023-01-16 18:19:53 +0900

    最終課題

--- saisyuu/saisyuu.js
+++ saisyuu/saisyuu.js
@@ -6,19 +6,18 @@ $('.slider').bxSlider({
     auto: 'true',
     pager: false
 });
-//任意のタブにURLからリンクするための設定
 function GethashID (hashIDName){
     if(hashIDName){
         //タブ設定
-        $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-            var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得
-            if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-                var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-                $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
-                $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
-                //表示させるエリア設定
-                $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
-                $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加
+        $('.tab li').find('a').each(function() {
+            var idName = $(this).attr('href');
+            if(idName == hashIDName){
+                var parentElm = $(this).parent();
+                $('.tab li').removeClass("active");
+                $(parentElm).addClass("active");
+
+                $(".area").removeClass("is-active");
+                $(hashIDName).addClass("is-active");
             }
         });
     }
@@ -26,44 +25,44 @@ function GethashID (hashIDName){
 
 //タブをクリックしたら
 $('.tab a').on('click', function() {
-    var idName = $(this).attr('href'); //タブ内のリンク名を取得
-    GethashID (idName);//設定したタブの読み込みと
-    return false;//aタグを無効にする
+    var idName = $(this).attr('href');
+    GethashID (idName);
+    return false;
 });
 
 
 
 
 
-//テキストのカウントアップ+バーの設定
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-    easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-    duration: 1000,//時間指定(1000＝1秒)
-    strokeWidth: 0.2,//進捗ゲージの太さ
-    color: '#555',//進捗ゲージのカラー
-    trailWidth: 0.2,//ゲージベースの線の太さ
-    trailColor: '#bbb',//ゲージベースの線のカラー
-    text: {//テキストの形状を直接指定
-        style: {//天地中央に配置
+
+var bar = new ProgressBar.Line(splash_text, {
+    easing: 'easeInOut',
+    duration: 1000,
+    strokeWidth: 0.2,
+    color: '#555',
+    trailWidth: 0.2,
+    trailColor: '#bbb',
+    text: {
+        style: {
             position: 'absolute',
             left: '50%',
             top: '50%',
             padding: '0',
-            margin: '-30px 0 0 0',//バーより上に配置
+            margin: '-30px 0 0 0',
             transform:'translate(-50%,-50%)',
             'font-size':'1rem',
             color: '#fff',
         },
-        autoStyleContainer: false //自動付与のスタイルを切る
+        autoStyleContainer: false
     },
     step: function(state, bar) {
-        bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+        bar.setText(Math.round(bar.value() * 100) + ' %');
     }
 });
 
-//アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-    $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+
+bar.animate(1.0, function () {
+    $("#splash").delay(500).fadeOut(800);
 });
 
 
